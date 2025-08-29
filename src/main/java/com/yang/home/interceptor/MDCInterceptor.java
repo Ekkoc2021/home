7271a89654e80178bb60f5112402c251a3c73a83
ekko <189890049@qq.com>
2025-08-29 22:53:09 +0800

    2025-8-29 04：实现日志链路追踪

--- src/main/java/com/yang/home/interceptor/MDCInterceptor.java
+++ src/main/java/com/yang/home/interceptor/MDCInterceptor.java
@@ -1,8 +1,8 @@
 package com.yang.home.interceptor;
 
+import com.yang.home.utils.LogReqIdUtil;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,7 +13,7 @@ public class MDCInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         // 在控制器执行之前调用
-
+        LogReqIdUtil.setMDCLogReqId();
         return true; // 继续执行
     }
 
@@ -24,8 +24,8 @@ public class MDCInterceptor implements HandlerInterceptor {
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-
         // 渲染完成，准备返回
+        LogReqIdUtil.removeMDCLogReqId();
     }
 
 }
---
+++ src/main/java/com/yang/home/utils/LogReqIdUtil.java
@@ -0,0 +1,28 @@
+package com.yang.home.utils;
+
+import org.slf4j.MDC;
+import java.util.UUID;
+
+public final class LogReqIdUtil {
+    private LogReqIdUtil(){
+
+    }
+
+    // 序列号key=>修改需要同步改配置文件
+    private static final String REQ_ID = "REQ_ID";
+
+    // 生成序号的
+    private static String generateId(){
+        return UUID.randomUUID().toString();
+    }
+
+    // 设置MDC
+    public static void setMDCLogReqId(){
+        String id = generateId();
+        MDC.put(REQ_ID,id);
+    }
+
+    public static void removeMDCLogReqId(){
+        MDC.remove(REQ_ID);
+    }
+}
