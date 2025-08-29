4838d18786115affb733308674ada936f0771694
ekko <189890049@qq.com>
2025-08-29 22:35:19 +0800

    2025-8-29 04：拦截器的使用！

---
+++ src/main/java/com/yang/home/interceptor/MDCInterceptor.java
@@ -0,0 +1,31 @@
+package com.yang.home.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class MDCInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 在控制器执行之前调用
+
+        return true; // 继续执行
+    }
+
+//    @Override
+//    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+//        // 控制器执行完毕之后，渲染视图之前
+//    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+        // 渲染完成，准备返回
+    }
+
+}
