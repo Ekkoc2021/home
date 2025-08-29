91586d8cd04bf01caac7d5c511c6f190c904a19c
ekko <189890049@qq.com>
2025-08-29 21:47:53 +0800

    2025-8-29 03：requestParam的使用

--- src/main/java/com/yang/home/HomeApplication.java
+++ src/main/java/com/yang/home/HomeApplication.java
@@ -22,38 +22,40 @@ public class HomeApplication {
     }
 
 
-
     @GetMapping("/hello")
-    public String test(){
+    public String test() {
 
         return "hello! The home project will start on here! ";
     }
 
     static final Logger logger = LoggerFactory.getLogger(HomeApplication.class.getName());
+
     @GetMapping("/log/{output}")
-    public String log(@PathVariable String output){
+    public String log(@PathVariable String output) {
 //        ALL < TRACE < DEBUG < INFO < WARN < ERROR < FATAL < OFF
 
-        logger.error("error {}",output);
-        logger.warn("debug {}",output);
-        logger.info("info {}",output);
-        logger.debug("debug {}",output);
-        logger.trace("trace {}",output) ;
+        logger.error("error {}", output);
+        logger.warn("debug {}", output);
+        logger.info("info {}", output);
+        logger.debug("debug {}", output);
+        logger.trace("trace {}", output);
 
-        return "日志级别测试:"+output;
+        return "日志级别测试:" + output;
     }
+
     public static final String REQ_ID = "REQ_ID";
+
     @GetMapping("/mdc")
-    public String mdc(@RequestParam("output") String test){ // 将output参数值赋予test,不指定参数按照名称绑定
+    public String mdc(@RequestParam("output") String test) { // 将output参数值赋予test,不指定参数按照名称绑定
         String req = UUID.randomUUID().toString();
         MDC.put(REQ_ID, req); // MDC是基于threadLocal实现的,记得移除
         logger.error("MDC error {}", test);
         logger.warn("MDC debug {}", test);
         logger.info("MDC info {}", test);
         logger.debug("MDC debug {}", test);
-        logger.trace("MDC trace {}", test) ;
+        logger.trace("MDC trace {}", test);
         MDC.remove(REQ_ID); // MDC是基于threadLocal实现的,记得移除
-        return "MDC 测试:"+req;
+        return "MDC 测试:" + req;
     }
 
     // LMWAYTQOJYHEYa2
